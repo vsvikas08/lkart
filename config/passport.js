d7963fcbf3cc415306369922aa8e368ba57c0a92
VIKAS KUMAR <52890062+vsvikas08@users.noreply.github.com>
2021-06-19 21:10:54 +0530

    Add files via upload

---
+++ config/passport.js
@@ -0,0 +1,43 @@
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// Load User Model
+var User = require('../models/user');
+
+
+
+module.exports = function(passport){
+	passport.use(
+		new LocalStrategy({usernameField:'email'},(email,password,done)=>{
+			// Match User
+			User.findOne({email:email})
+				.then(user => {
+					if(!user){
+						return done(null,false,{message:'That email is not registered!'});
+					}
+					// Match password
+					bcrypt.compare(password,user.password,(err,isMatch)=>{
+						if(err)
+							throw err;
+						if(isMatch){
+							return done(null,user);
+						}else{
+							return done(null,false,{message:"Password Incorrect"});
+						}
+					});
+				})
+				.catch(err => console.log(err));
+		})
+	);
+
+	passport.serializeUser(function(user, done) {
+  		done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+  		User.findById(id, function(err, user) {
+    		done(err, user);
+  		});
+	});
+}
